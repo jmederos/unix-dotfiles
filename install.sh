959d0e3ce6ca04aee5684005bb0c403b6c69be36
Jacob Mederos <jacob@vizzario.com>
2017-09-28 15:31:07 -0700

    ✨Initial Commit✨

---
+++ install.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+unalias cp
+
+# vim
+cp -rf ./vim/ ~/
+# tmux
+cp -rf ./tmux ~/
+
+echo "done"
---
+++ packageConfig.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+OUTPUT_FOLDER=unix_config
+
+# Make folders 
+mkdir -p ./$OUTPUT_FOLDER/vim/.vim
+mkdir -p ./$OUTPUT_FOLDER/tmux/.vim
+
+# Copy vim configuration and dependencies
+cp -Rp ~/.vim ./$OUTPUT_FOLDER/vim/.vim/
+cp -a ~/.vimrc ./$OUTPUT_FOLDER/vim/.vimrc
+
+# Copy tmux configuration
+cp -a ~/.tmux.conf ./$OUTPUT_FOLDER/tmux/.tmux.conf
+
+# Copy install script
+cp ./install.sh ./$OUTPUT_FOLDER/
+
+# zip folder and remove tmp
+zip -r $OUTPUT_FOLDER.zip $OUTPUT_FOLDER
+rm -rf ./$OUTPUT_FOLDER
